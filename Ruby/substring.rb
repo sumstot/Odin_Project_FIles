d27fa1c43dc9970aa916a450a72ae5abeb7102f7
Soren Umstot <soren.umstot@gmail.com>
2023-01-28 10:07:29 +0900

    Initial commit for the repository

---
+++ Ruby/substring.rb
@@ -0,0 +1,8 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substring(str, dictionary)
+ result = Hash.new(0)
+
+end
+
+substring("below", dictionary)
