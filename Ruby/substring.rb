7348172f49a309fa6a9689ff566af1fd17e1af6d
Soren Umstot <soren.umstot@gmail.com>
2023-01-29 12:30:40 +0900

    finished substring challenge

--- Ruby/substring.rb
+++ Ruby/substring.rb
@@ -1,8 +1,11 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substring(str, dictionary)
- result = Hash.new(0)
-
+  results = Hash.new(0)
+  dictionary.each do |word|
+    results[word] = str.downcase.scan(/(?=#{word})/).count if str.downcase.include?(word)
+  end
+  results
 end
 
-substring("below", dictionary)
+p substring("below", dictionary)
