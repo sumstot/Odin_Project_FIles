3371e39dd4883049c3eed9e9654522b066d04f88
Soren Umstot <soren.umstot@gmail.com>
2023-01-29 19:23:29 +0900

    Fixed styling issues

--- Ruby/bubble_sort.rb
+++ Ruby/bubble_sort.rb
@@ -1,10 +1,10 @@
-list = [10, 2, 3, 1, 8, 4, 2]
+list = [4,3,78,2,0,2]
 
 def bubble_sort(list)
   n = list.length
   loop do
     swapped = false
-    (n-1).times do |i|
+    (n - 1).times do |i|
       if list[i] > list[i + 1]
         list[i], list[i + 1] = list[i + 1], list[i]
         swapped = true
